01509472e673f81963653c13b57cb135a77b55c7
mayur <mayur.hansinfotech@gmail.com>
2022-03-05 17:32:50 +0530

    detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HttpHeaders, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { GaugeModule } from 'angular-gauge';
 
 import {MatTabsModule} from '@angular/material/tabs';
 import { MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule} from '@angular/material/select';
-import { SearchBarComponent } from './component/search-bar/search-bar.component'
+import { SearchBarComponent } from './component/search-bar/search-bar.component';
+import { HomeComponent } from './component/home/home.component'
+import { HttpHeadersInterceptor } from './interceptors/http-headers-interceptor';
+import { HttpErrorsInterceptor } from './interceptors/http-erros-interceptor';
+import { DetailsComponent } from './component/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
+    HomeComponent,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,7 +37,18 @@ import { SearchBarComponent } from './component/search-bar/search-bar.component'
     MatTabsModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
